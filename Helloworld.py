b4de57401beed3ad0197d6e844589e3f0d972f82
AnjelIoannou <151517655+AnjelIoannou@users.noreply.github.com>
2024-03-26 15:44:52 +0000

    Update Helloworld.py

--- Helloworld.py
+++ Helloworld.py
@@ -3,3 +3,6 @@ name=input("please enter your name")
 print(name)
 surname=input("please enter your surname")
 print(surname)
+email_address=input("please enter your email address")
+print(email_address)
+
